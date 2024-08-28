6faf53815f479ef2cb4abdedf0262b357e3d0f63
MaximoBadeigts <maximobadeigts05@gmail.com>
2024-08-28 16:20:28 -0300

    feat: actividad 1

--- actividades/ej_01/app.py
+++ actividades/ej_01/app.py
@@ -1,1 +1,14 @@
-# Aplicacion del servidor
+# Aplicaciofrom microdot import Microdot, send_file
+
+do_connect()
+app = Microdot()
+
+@app.route('/')
+async def index(request):
+    return send_file('index.html')
+
+@app.route('/<dir>/<file>')
+async def index(request, dir, file):
+    return send_file("/" + dir+ "/" + file)
+
+app.run(port=80)n del servidor
